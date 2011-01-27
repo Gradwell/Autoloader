66316c2584e7cac95859db899668fb736dac8fda
Stuart Herbert <stuart@stuartherbert.com>
2011-01-27 12:47:11 +0000

    First commit

---
+++ src/php/gwc.autoloader.php
@@ -0,0 +1,91 @@
+<?php
+
+// autoloader support
+function __gwc_normalise_path($className)
+{
+	 $fileName  = '';
+	 $lastNsPos = strripos($className, '\\');
+	  
+	 if ($lastNsPos !== false)
+	 {
+	 	$namespace = substr($className, 0, $lastNsPos);
+	        $className = substr($className, $lastNsPos + 1);
+	        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	 }
+	
+         return $fileName . str_replace('_', DIRECTORY_SEPARATOR, $className);
+
+}
+
+function __gwc_init_namespace($namespace, $fileExt = '.init.php')
+{
+        static $loadedNamespaces = array();
+
+        // have we loaded this namespace before?
+        if (isset($loadedNamespaces[$namespace]))
+        {
+                // yes we have - bail
+                return;
+        }
+
+        $path = __gwc_normalise_path($namespace);
+        $filename = $path . '/_init/' . end(explode('/', $path)) . $fileExt;
+        $loadedModules[$namespace] = $filename;
+
+        __gwc_include($filename);
+}
+
+function __gwc_init_tests($namespace)
+{
+        __gwc_namespace($namespace, '.initTests.php');
+}
+
+function __gwc_autoload($classname)
+{
+        if (class_exists($classname) || interface_exists($classname))
+        {
+                return FALSE;
+        }
+
+        // convert the classname into a filename on disk
+        $classFile = __gwc_normalise_path($classname) . '.php';
+
+        return __gwc_include($classFile);
+}
+
+function __gwc_include($filename)
+{
+       	$pathToSearch = explode(PATH_SEPARATOR, get_include_path());
+
+        // keep track of what we have tried; this info may help other
+        // devs debug their code
+        $failedFiles = array();
+
+        foreach ($pathToSearch as $searchPath)
+        {
+		$fileToLoad = $searchPath . '/' . $filename;
+		// var_dump($fileToLoad);
+                if (!file_exists($fileToLoad))
+                {
+                        $failedFiles[] = $fileToLoad;
+                        continue;
+                }
+
+                require($fileToLoad);
+                return TRUE;
+        }
+
+        // if we get here, we could not find the requested file
+        // we do not die() or throw an exception, because there may
+        // be other autoload functions also registered
+        return FALSE;
+}
+
+function __gwc_autoload_alsoSearch($dir)
+{
+	set_include_path($dir . PATH_SEPARATOR . get_include_path());
+}
+
+spl_autoload_register('__gwc_autoload');
+
+?>
